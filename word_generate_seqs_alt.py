19a4eb263b5948680e96df9ea569ff58820f4c51
Johnny Wong <johnnywong@Johnnys-MBP.hsd1.va.comcast.net>
2020-05-06 22:57:45 -0400

    5 word autocomplete

---
+++ word_generate_seqs_alt.py
@@ -0,0 +1,45 @@
+import random
+from word_tools import load_doc, clean_line, save_doc
+
+in_filename = 'data/aol/full/train.query.txt'
+doc = load_doc(in_filename)
+lines = doc.split('\n')
+# print(doc[:200])
+
+####
+# NOTE: We remove terms of size 1 because makes no sense for word prediction
+####
+
+print('Filtering, Normalizing, and Creating Sequences out of tokens... ')
+sequences = list()
+for seq in lines:
+    seq_length = seq.split()
+
+    # Keep ONLY sequences of length 5 
+    if len(seq_length) == 5:
+        tokens = clean_line(seq)
+        # convert into a line
+        line = ''.join(tokens)
+        # store
+        sequences.append(line)
+
+    # # Shorten sequences of length 5
+    # if len(seq_length) >= 5:
+    #     tokens = clean_line(seq)
+    #     line = ' '
+    #     iter = 0
+    #     for tok in tokens:
+    #         if iter >= 5:
+    #             break
+    #         line = line.join(tok)
+    #         iter += 1
+    #     sequences.append(line)
+
+
+print('Total Sequences: %d' % len(sequences))
+
+print('Saving Sequences to a File')
+# save sequences to file
+out_file_name = 'data/aol/full/train.query.sequences.alt2.txt'
+save_doc(sequences, out_file_name)
+print('Saved... :)')
--- word_main_alt.py
+++ word_main_alt.py
@@ -11,35 +11,34 @@ from word_tools import load_doc
 import numpy as np
 
 # load doc
-in_filename = 'data/aol/full/train.query.sequences.txt'
+in_filename = 'data/aol/full/train.query.sequences.alt2.txt'
 # in_filename = 'data/aol/full/temp_check.txt'
 doc = load_doc(in_filename)
 lines = doc.split('\n')
 
+# Trim and keep only length of 5
+trim_lines = []
+for item in lines:
+    if len(item.split()) == 5:
+        trim_lines.append(item)
+
 # integer encode sequences of words
 tokenizer = Tokenizer()
-tokenizer.fit_on_texts(lines) # Updates internal vocabulary based on a list of texts.
-sequences = tokenizer.texts_to_sequences(lines) # Transforms each text in texts to a sequence of integers.
+tokenizer.fit_on_texts(trim_lines) # Updates internal vocabulary based on a list of texts.
+sequences = tokenizer.texts_to_sequences(trim_lines) # Transforms each text in texts to a sequence of integers.
 # vocabulary size
 vocab_size = len(tokenizer.word_index) + 1
 
-# Get length of longest term
-max_length = max(len(seq) for seq in sequences)
-for seq in sequences:
-    if len(seq) != max_length:
-        pad_length = max_length - len(seq)
-        for i in range(pad_length):
-            seq.append(0)
-print('Max Length: ', max_length)
 # separate into input and output
 sequences = array(sequences)
+print(sequences)
 X, y = sequences[:,:-1], sequences[:,-1]
 y = to_categorical(y, num_classes=vocab_size)
 seq_length = X.shape[1]
 
 # define model
 model = Sequential()
-model.add(Embedding(vocab_size, max_length, input_length=seq_length))
+model.add(Embedding(vocab_size, 5, input_length=seq_length))
 model.add(LSTM(100, return_sequences=True))
 model.add(LSTM(100))
 model.add(Dense(100, activation='relu'))
